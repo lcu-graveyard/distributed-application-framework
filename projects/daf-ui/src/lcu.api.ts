808b4c00284d84ba84949ac0cce2f7b1428f82d2
Michael Gearhardt <michael.gearhardt@fathym.com>
2018-12-20 00:16:15 -0700

    DAF UI Module export

--- projects/daf-ui/src/lcu.api.ts
+++ projects/daf-ui/src/lcu.api.ts
@@ -2,6 +2,7 @@ export * from './lib/component-selector/component-selector.dialog';
 export * from './lib/component-selector/component-selector.module';
 export * from './lib/control-builder/control-builder.dialog';
 export * from './lib/control-builder/control-builder.module';
+export * from './lib/daf-ui.module';
 export * from './lib/data-grid.api';
 export * from './lib/display/display.directive';
 export * from './lib/display/display.module';
