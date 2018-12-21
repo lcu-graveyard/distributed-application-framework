ed7e3f316f4eb85be87f960403a889bfcfe36647
xchanin <me@shannonbruns.com>
2018-12-20 18:17:10 -0700

    update column definitions and toggle for icons, and data

--- projects/daf-ui/src/lib/data-grid/config/column-def.config.ts
+++ projects/daf-ui/src/lib/data-grid/config/column-def.config.ts
@@ -4,52 +4,54 @@ export class ColumnDefinition implements IColumnConfigModel {
   public colType: string;
   public icon: string;
   public title: string;
-  public sortable: boolean;
+  public sortable?: boolean;
   public pipe?: string;
-  public value: (any) => any;
-  public valueAsIcon: boolean;
-  public columnData: Object = {};
-  public iconConfigFunc;
+  public value: string;
+  public showIcon: boolean;
+  public showValue: boolean;
+  public iconConfigFunc?: Function;
 
 /**
- * Constructor for ColumnDefinition
+ * Constructor for Grid column definitions
  * @param colType Column data type
  * @param title Column title
  * @param value Function that passes in the column object and then returns the value, based of def
- * @param valueAsIcon Boolean for toggling icons instead of showing data
+ * @param showValue Boolean for toggling icons
+ * @param showIcon Boolean for toggling icons
  * @param sortable Allow column to be sorted
  * @param pipe String value of pipe to use
  * @param iconConfigFunc Callback function for setting icons
  */
 
   constructor(colType: string,
-              title: string, value: (any) => any,
-              valueAsIcon: boolean,
+              title: string,
+              showValue: boolean,
+              showIcon: boolean,
               sortable?: boolean,
               pipe?: string,
               iconConfigFunc?: Function) {
 
     this.colType = colType;
     this.title = title;
-    this.value = (colObj) => colObj[colType];
-    this.valueAsIcon = valueAsIcon;
+    this.showValue = showValue;
+    this.showIcon = showIcon;
     this.sortable = sortable;
     this.pipe = pipe;
     this.iconConfigFunc = iconConfigFunc;
   }
 
   /**
-   * This will show an icon instead of a data value
-   * @param val item being returned for each column
+   * Set values and toggle icons and data values on and off
+   * @param colObj Each item coming from the grid rows
    */
-  public setColumnData<T>(val: T): void {
-    this.columnData = val;
+  public setValue<T>(colObj: T): void {
 
-    /**
-     * Set icon, based of callback function from the 'parent' class
-     */
-    if (this.iconConfigFunc) {
-     this.icon = this.iconConfigFunc(this.columnData, this.colType);
+    if (this.showIcon && this.iconConfigFunc) {
+      this.icon = this.iconConfigFunc(colObj, this.colType);
+    }
+
+    if (this.showValue) {
+      this.value = colObj[this.colType];
     }
   }
 }
--- projects/daf-ui/src/lib/data-grid/models/column-config.model.ts
+++ projects/daf-ui/src/lib/data-grid/models/column-config.model.ts
@@ -2,9 +2,10 @@ export interface IColumnConfigModel {
   colType: string;
   icon: string;
   title: string;
-  value: (any) => any;
-  valueAsIcon: boolean;
+  value: string;
+  showIcon: boolean;
+  showValue: boolean;
   sortable?: boolean;
   pipe?: string;
   iconConfigFunc?: Function;
-  }
+}
