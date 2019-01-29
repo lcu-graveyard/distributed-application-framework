7804e1f08d4db9a87d611f17114f06354c28dc54
ttrichar <trevor.richardson@fathym.com>
2019-01-29 12:59:50 -0700

    Commit

--- projects/daf-common/src/lib/contexts.api.ts
+++ projects/daf-common/src/lib/contexts.api.ts
@@ -10,5 +10,4 @@ export * from './contexts/pages-setup.context';
 export * from './contexts/pointers-config.context';
 export * from './contexts/provisioning-config.context';
 export * from './contexts/reporting-config.context';
-export * from './contexts/login-provider-config.context';
 export * from './contexts/solutions-setup.context';
